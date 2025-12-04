1fc61a2f4c1aad134f46e2d6557ddbc320ef1b4f
pener <rinyguy@gmail.com>
2025-12-04 04:15:14 +0100

    improved world aligning

--- examples/test.asm
+++ examples/test.asm
@@ -2,19 +2,26 @@ v_blank:
     .i8	    0x00
 
 _start:
-    add.pc	r0, ._xhwi_handler
-    ctl.w	xhwi, r0
-    add.pc	r0, .v_blank
+    add.pc	    r0, ._xhwi_handler
+    ctl.w	    xhwi, r0
+    #add.pc	    r0, ._xswi_handler
+    #ctl.w	    xswi, r0
+
+_loop:
+    # game logic here
 
 _wait_for_vblank:
+    add.pc	    r0, .v_blank
     ldr.u8	    r1, r0
     bra.eq	    rZ, r1, ._wait_for_vblank
 
+    jmp         rZ, ._loop
+
 _xhwi_handler:
-    # mov     r1, 1
-    # add.pc	r0, .v_blank
-    # str.i8	r1, r0
+    mov         r1, 1
+    add.pc	    r0, .v_blank
+    str.i8	    r1, r0
     irq.ret
 
-# _xswi_handler:
-#     irq.ret
+#_xswi_handler:
+#    irq.ret
